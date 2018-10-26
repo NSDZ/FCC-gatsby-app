051a1f2bf8898520a8b8aa68630c71e4f261dae0
Nassim Sbaihia <nassim.sbaihia@gmail.com>
2018-10-26 01:14:28 +0100

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import Image from '../components/image'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>//Hello, world !!</h1>
+    <p>it's Nassim, a computer science student and a javascript lover;</p>
+    <p>Now,let's hack a thoun;</p>
+    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage
